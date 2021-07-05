d4bd75335a4732fc0211c7cf3f2f73e932f02383
CNDvn <banhduchieucode@gmail.com>
2021-07-05 20:49:45 +0700

    start

---
+++ Learn-SpringBoot-techmaster/src/main/java/com/cnd/voc/exception/CustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.cnd.voc.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+@RestControllerAdvice
+public class CustomExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handlerNotFoundException(NotFoundException ex, WebRequest req){
+        return new ErrorResponse(HttpStatus.NOT_FOUND, ex.getMessage());
+    }
+}
